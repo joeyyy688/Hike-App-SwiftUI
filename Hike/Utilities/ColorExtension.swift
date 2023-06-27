f63e431a6e7871d3e1078862a35273eebed9c959
joeyyy688 <edinjoey@gmail.com>
2023-06-27 22:12:08 +0000

    initialized project, created custom card

---
+++ Hike/Utilities/ColorExtension.swift
@@ -0,0 +1,19 @@
+//
+//  ColorExtension.swift
+//  Hike
+//
+//  Created by Joseph on 6/27/23.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color{
+    static let customGreenLight = Color("ColorGreenLight")
+    static let customGreenMedium = Color("ColorGreenMedium")
+    static let customGreenDark = Color("ColorGreenDark")
+    static let customGrayLight = Color("ColorGrayLight")
+    static let customGrayMedium = Color("ColorGrayMedium")
+    static let customIndigoLight = Color("ColorIndigoLight")
+    static let customSalmonLight = Color("ColorSalmonLight")
+}
