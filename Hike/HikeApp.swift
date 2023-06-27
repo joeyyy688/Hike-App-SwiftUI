24cfa49b33aba0d44c8c2fd83b465a8f8cdf9385
joeyyy688 <edinjoey@gmail.com>
2023-06-27 19:21:20 +0000

    Initial Commit

---
+++ Hike/HikeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikeApp.swift
+//  Hike
+//
+//  Created by Joseph on 6/27/23.
+//
+
+import SwiftUI
+
+@main
+struct HikeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
