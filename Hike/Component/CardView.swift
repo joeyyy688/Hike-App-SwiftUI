2ad6dc8229da3ed6bd98e4151c3ae81e3eea15a8
joeyyy688 <edinjoey@gmail.com>
2023-06-28 00:34:05 +0000

    created hiking card

--- Hike/Component/CardView.swift
+++ Hike/Component/CardView.swift
@@ -12,19 +12,61 @@ struct CardView: View {
         ZStack {
             CustomBackgroundView()
             
-            ZStack {
-                Circle().fill(
-                    LinearGradient(
-                        colors: [Color("ColorIndigoMedium"),Color("ColorSalmonLight")],
-                        startPoint: .topLeading, endPoint: .bottomTrailing
+            VStack {
+                
+                VStack(alignment: .leading) {
+                    
+                    HStack (spacing: 30){
+                        Text("Hiking")
+                            .font(.system(size: 53))
+                            .fontWeight(.black)
+                            .foregroundStyle(
+                                LinearGradient(
+                                    colors: [Color.customGrayLight, Color.customGrayMedium,],
+                                    startPoint: .top, endPoint: .bottom
+                                )
+                            )
+                        
+                        Button(action: {
+                            
+                        }, label: {
+                            CustomButtonView()
+                        })
+                    }//: HSTACK
+                    
+                    Text("Fun and enjoyable outdoor activity for friends and families.")
+                        .multilineTextAlignment(.leading)
+                        .italic()
+                        .foregroundColor(.customGrayMedium)
+                        .fontWeight(.light)
+                        .padding(EdgeInsets(top: 0, leading: 10, bottom: 0, trailing: 10))
+                        
+                }//: VSTACK
+                .padding(.horizontal, 30)
+                
+                ZStack {
+                    Circle().fill(
+                        LinearGradient(
+                            colors: [Color("ColorIndigoMedium"),Color("ColorSalmonLight")],
+                            startPoint: .topLeading, endPoint: .bottomTrailing
+                        )
                     )
-                )
-                .frame(width: 256, height: 256)
+                    .frame(width: 256, height: 256)
+                    
+                    
+                    Image("image-1")
+                        .resizable()
+                    .scaledToFit()
+                    
+                }
                 
+                Button(action: {
+                    
+                }, label: {
+                    Text("Explore More")
+                })
+                .buttonStyle(.borderedProminent)
                 
-                Image("image-1")
-                    .resizable()
-                .scaledToFit()
                 
             } //: ZSTACK
         }// ZSTACK - CARD
---
+++ Hike/Component/CustomButtonView.swift
@@ -0,0 +1,35 @@
+//
+//  CustomButtonView.swift
+//  Hike
+//
+//  Created by Joseph on 6/27/23.
+//
+
+import SwiftUI
+
+struct CustomButtonView: View {
+    var body: some View {
+        ZStack{
+            Circle().fill(
+                LinearGradient(colors: [.white, .customGreenLight, .customGreenMedium], startPoint: .top, endPoint: .bottom)
+            )
+            
+            Circle().stroke(LinearGradient(colors: [.customGrayLight, .customGrayMedium], startPoint: .top, endPoint: .bottom), lineWidth: 4)
+            
+            Image(systemName: "figure.hiking")
+                .font(.system(size: 30))
+                .fontWeight(.black)
+                .foregroundStyle(LinearGradient(colors: [ .customGrayLight, .customGrayMedium], startPoint: .top, endPoint: .bottom))
+                
+        }
+        .frame(width: 58, height: 58)
+    }
+}
+
+struct CustomButtonView_Previews: PreviewProvider {
+    static var previews: some View {
+        CustomButtonView()
+            .previewLayout(.sizeThatFits)
+            .padding()
+    }
+}
